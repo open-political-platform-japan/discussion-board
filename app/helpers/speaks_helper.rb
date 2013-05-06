cedce336f214df34587f806ee72fa689c36fb396
Ohkubo KOHEI <ohkubo@magician.jp>
2013-05-07 03:33:29 +0900

    close #6

--- app/helpers/speaks_helper.rb
+++ app/helpers/speaks_helper.rb
@@ -1,2 +1,9 @@
 module SpeaksHelper
+  def with_speak_link(str)
+    str.gsub(/#(\d+)/) do
+      target = Speak.find_by_id($1)
+      next $& unless target
+      link_to($&, speak_path($1), title: truncate(target.text) )
+    end.html_safe
+  end
 end
