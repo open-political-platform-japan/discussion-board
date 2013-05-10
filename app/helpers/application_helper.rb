0da367ecaacd9251b37787222b8b5a362d14b48a
Ohkubo KOHEI <ohkubo@magician.jp>
2013-05-10 20:20:50 +0900

    発言一覧をテーブルではなく 2ch 風に

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def vote_icon(speak)
-    return unless can? :vote, speak
-    if current_user.liked? speak
-      link_to glyph(:thumbs_up), vote_speak_path(speak, cancel: true), method: :post
-    else
-      link_to glyph(:thumbs_up, :gray), vote_speak_path(speak), method: :post
-    end
-  end
 end
--- app/helpers/speaks_helper.rb
+++ app/helpers/speaks_helper.rb
@@ -1,4 +1,12 @@
 module SpeaksHelper
+  def vote_icon(speak)
+    return unless can? :vote, speak
+    if current_user.liked? speak
+      link_to glyph(:thumbs_up, :active), vote_speak_path(speak, cancel: true), method: :post
+    else
+      link_to glyph(:thumbs_up), vote_speak_path(speak), method: :post
+    end
+  end
   def with_speak_link(str)
     str.gsub(/#(\d+)/) do
       target = Speak.find_by_id($1)
