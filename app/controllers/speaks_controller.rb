21ac287a6dcfe6b913664ca1baf798aa1a3dfa79
Ohkubo KOHEI <ohkubo@magician.jp>
2013-05-11 19:47:36 +0900

    DB アクセス最適化 (うまく動いてない？？)

--- app/controllers/speaks_controller.rb
+++ app/controllers/speaks_controller.rb
@@ -6,15 +6,15 @@ class SpeaksController < ApplicationController
 
   # GET /speaks
   def index
-    @q = Speak.includes(:user).search(params[:q])
+    @q = Speak.includes(:user, :votes).search(params[:q])
     if params[:q]
       @q = Speak.includes(:user).search(params[:q])
       @speaks = @q.result(:distinct => true)
     elsif params[:order] == 'votes'
-      @speaks = Speak.includes(:user).order('cached_votes_score DESC')
+      @speaks = Speak.includes(:user, :votes).order('cached_votes_score DESC')
       @speaks = @speaks.where("? <= id", Configurable.speak_from) if Configurable.speak_from.to_i > 0
     else
-      @speaks = Speak.includes(:user).order("created_at DESC")
+      @speaks = Speak.includes(:user, :votes).order("created_at DESC")
     end
   end
 
