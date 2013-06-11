94d6e63820a49e0fc1f58f4d27d77bffc1f1c33e
Ohkubo KOHEI <ohkubo@magician.jp>
2013-06-11 10:32:13 +0900

    update heroku auto deploy script

--- script/travis/deploy.rb
+++ script/travis/deploy.rb
@@ -8,5 +8,10 @@ unless ENV['TRAVIS_PULL_REQUEST'] == 'true'
   sh 'wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh'
   sh 'git remote add heroku git@heroku.com:neetbe.git'
   sh 'yes | heroku keys:add'
+
+  sh 'echo "Host heroku.com" >> ~/.ssh/config'
+  sh 'echo "   StrictHostKeyChecking no" >> ~/.ssh/config'
+  sh 'echo "   CheckHostIP no" >> ~/.ssh/config'
+  sh 'echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config'
   sh 'yes | git push heroku master'
 end
